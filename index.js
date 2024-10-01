1900fbd55f4c3a92c8d44c7d9b472793bcc7d5a4
joyce wamocho <joywamo@gmail.com>
2024-10-01 14:06:49 +0300

    map method lab

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased =() => {
+  return tutorials.map(tutorial => {
+    return tutorial
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1)) 
+    .join(" ");
+
+});
+  return titleCased;
+
+};
+
+console.log(titleCased);
+
+   
+
+
+
+
